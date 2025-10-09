5fa1266ae2396ccb5e374dce4db230b3f763dfd3
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-10-09 13:27:00 -0400

    Handle edge case: tests run before setup

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
-set -uxo pipefail
+set -uo pipefail
+
+if [ ! -f venv/bin/pytest ]; then
+	echo
+	echo "ERROR:"
+	echo "    The file venv/bin/pytest couldn't be found!"
+	echo "    Be sure to run ./setup.sh first, resolving any errors that occur."
+	echo
+	exit
+fi
 
 venv/bin/pytest
 
