9f12dd0e249b10d8cab0191abd47de66f62a4b2b
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-10-09 12:11:15 -0400

    Simplify test suite

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -22,6 +22,8 @@ def assert_file_contents(file_path, expected):
         assert observed == expected
 
 def test_basic_functionality():
+    hashes = "hashes.csv"
+    linkages = "linkages.csv"
     with tempfile.TemporaryDirectory() as temp_dir:
         pprl._create_CLKs(
                 data_folder = test_data_folder,
@@ -29,27 +31,33 @@ def test_basic_functionality():
                 schema_folder = test_schema_folder,
                 secret = "secret.txt",
                 output_folder = temp_dir,
-                output = "CLKs.csv",
+                output = hashes,
                 quiet=True)
         pprl._match_CLKs(
                 data_folder = temp_dir,
-                hashes = ["CLKs.csv"],
+                hashes = [hashes],
                 output_folder = temp_dir,
-                output = "abc",
+                output = linkages,
                 quiet=True)
         assert_file_contents(
-                os.path.join(temp_dir, "abc"),
+                os.path.join(temp_dir, linkages),
                 'zoo,zoo\n'
                 )
 
 def test_basic_ordering():
+    hashes_1 = "hashes1.csv"
+    hashes_2 = "hashes2.csv"
+    linkages = "linkages.csv"
+    schema = "schema.json"
+    secret = "secret.txt"
+    patients = "3_test_patients.csv"
     with tempfile.TemporaryDirectory() as temp_dir:
         pprl._create_CLKs(
                 data_folder = test_data_folder,
-                patients = "3_test_patients.csv",
-                schema = "schema.json",
-                secret = "secret.txt",
-                output = "CLKsa1.csv",
+                patients = patients,
+                schema = schema,
+                secret = secret,
+                output = hashes_1,
                 output_folder = temp_dir,
                 quiet=True)
         pprl._create_CLKs(
@@ -57,49 +65,53 @@ def test_basic_ordering():
                 patients = "rev_3_test_patients.csv",
                 schema = "schema.json",
                 secret = "secret.txt",
-                output = "CLKsa2.csv",
+                output = hashes_2,
                 output_folder = temp_dir,
                 quiet=True)
         pprl._match_CLKs(
                 data_folder = temp_dir,
-                hashes = ['CLKsa1.csv', 'CLKsa2.csv'],
+                hashes = [hashes_1, hashes_2],
                 threshold = 0.9,
-                output = 'matches.csv',
+                output = linkages,
                 output_folder = temp_dir,
                 quiet=True)
         assert_file_contents(
-                os.path.join(temp_dir, "matches.csv"),
+                os.path.join(temp_dir, linkages),
                 'zoo,zoo\n0,2\n1,1\n2,0\n'
                 )
 
 def test_additional_ordering():
+    schema = "20_ordering.json"
+    secret = "secret.txt"
+    hashes_1 = "hashes1.csv"
+    hashes_2 = "hashes2.csv"
+    linkages = "linkages.csv"
     with tempfile.TemporaryDirectory() as temp_dir:
         pprl._create_CLKs(
                 data_folder = test_data_folder,
                 schema_folder = test_schema_folder,
                 patients = "20_test_matches_a.csv",
-                schema = "20_ordering.json",
-                secret = "secret.txt",
+                schema = schema,
+                secret = secret,
                 output_folder = temp_dir,
-                output = 'asd1',
+                output = hashes_1,
                 quiet=True)
         pprl._create_CLKs(
-                data_folder = 'tests/data',
-                schema_folder = 'tests/schemas',
+                data_folder = test_data_folder,
+                schema_folder = test_schema_folder,
                 patients = "20_test_matches_b.csv",
-                schema = "20_ordering.json",
-                secret = "secret.txt",
+                schema = schema,
+                secret = secret,
                 output_folder = temp_dir,
-                output = 'asd2',
+                output = hashes_2,
                 quiet=True)
         pprl._match_CLKs(
-                hashes = ['asd1','asd2'],
+                hashes = [hashes_1, hashes_2],
                 data_folder = temp_dir,
-                threshold = 0.9,
                 output_folder = temp_dir,
-                output = "qmatches.csv", # TODO: tempfile
+                output = linkages,
                 quiet=True)
         assert_file_contents(
-                os.path.join(temp_dir, "qmatches.csv"),
+                os.path.join(temp_dir, linkages),
                 'simple_synthetic,simple_synthetic\n0,0\n1,2\n2,4\n3,6\n4,8\n5,10\n6,12\n7,14\n8,16\n9,18\n'
                 )
