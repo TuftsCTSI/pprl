7bb008838ffd01e2f814f857732c04572fc3bbde
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-10-09 15:11:33 -0400

    Update scripts

--- runtests.sh
+++ runtests.sh
@@ -7,6 +7,7 @@ if [ ! -f venv/bin/pytest ]; then
 	echo "    The file venv/bin/pytest couldn't be found!"
 	echo "    Be sure to run ./setup.sh first, resolving any errors that occur."
 	echo
+
 	exit
 fi
 
