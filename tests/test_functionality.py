39b5f9bb04fba7102b5427a28bf7cb9f26428003
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-10-09 12:18:12 -0400

    Simplify test suite

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -8,11 +8,6 @@ import tempfile
 sys.path.append('./src')
 import pprl
 
-current_dir = os.getcwd()
-#parent_dir = os.path.dirname(current_dir)
-test_data_folder = os.path.join(current_dir, "tests/data")
-test_schema_folder = os.path.join(current_dir, "tests/schemas")
-
 def assert_file_comparison(file_path_1, file_path_2):
     assert filecmp.cmp(file_path_1, file_path_2)
 
@@ -20,6 +15,52 @@ def assert_file_contents(file_path, expected):
     with open(file_path,'r') as file:
         observed = file.read()
         assert observed == expected
+
+def basic_test_pattern(
+    patients_1 = None,
+    patients_2 = None,
+    expected_linkages = None,
+    hashes_1 = "hashes1.csv",
+    hashes_2 = "hashes2.csv",
+    linkages = "linkages.csv",
+    schema = "schema.json",
+    secret = "secret.txt",
+    threshold = 0.9
+    ):
+
+    current_dir = os.getcwd()
+    #parent_dir = os.path.dirname(current_dir)
+    test_data_folder = os.path.join(current_dir, "tests/data")
+    test_schema_folder = os.path.join(current_dir, "tests/schemas")
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        pprl._create_CLKs(
+                data_folder = test_data_folder,
+                patients = patients_1,
+                schema = schema,
+                secret = secret,
+                output = hashes_1,
+                output_folder = temp_dir,
+                quiet=True)
+        pprl._create_CLKs(
+                data_folder = test_data_folder,
+                patients = patients_2,
+                schema = schema,
+                secret = secret,
+                output = hashes_2,
+                output_folder = temp_dir,
+                quiet=True)
+        pprl._match_CLKs(
+                data_folder = temp_dir,
+                hashes = [hashes_1, hashes_2],
+                threshold = threshold,
+                output = linkages,
+                output_folder = temp_dir,
+                quiet=True)
+        assert_file_contents(
+                os.path.join(temp_dir, linkages),
+                expected_linkages
+                )
 
 def test_basic_functionality():
     hashes = "hashes.csv"
