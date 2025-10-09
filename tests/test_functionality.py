352d351fcfac2d21904de6a2a24aa0276fca7179
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-10-09 12:55:19 -0400

    Format

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -8,6 +8,10 @@ import tempfile
 sys.path.append('./src')
 import pprl
 
+# ----------------------------------------------------------
+# Test utility functions
+# ----------------------------------------------------------
+
 def assert_file_comparison(file_path_1, file_path_2):
     assert filecmp.cmp(file_path_1, file_path_2)
 
@@ -16,19 +20,23 @@ def assert_file_contents(file_path, expected):
         observed = file.read()
         assert observed == expected
 
+# ----------------------------------------------------------
+# Templates and patterns for tests
+# ----------------------------------------------------------
+
 def basic_test_pattern(
-    patients_1 = None,
-    patients_2 = None,
-    expected_linkages = None,
-    hashes_1 = "hashes1.csv",
-    hashes_2 = "hashes2.csv",
-    linkages = "linkages.csv",
-    schema = "schema.json",
-    secret = "secret.txt",
-    threshold = 0.9,
-    data_folder = None,
-    schema_folder = None,
-    ):
+        patients_1 = None,
+        patients_2 = None,
+        expected_linkages = None,
+        hashes_1 = "hashes1.csv",
+        hashes_2 = "hashes2.csv",
+        linkages = "linkages.csv",
+        schema = "schema.json",
+        secret = "secret.txt",
+        threshold = 0.9,
+        data_folder = None,
+        schema_folder = None,
+        ):
 
     current_dir = os.getcwd()
     #parent_dir = os.path.dirname(current_dir)
@@ -72,23 +80,27 @@ def basic_test_pattern(
                 expected_linkages
                 )
 
+# ----------------------------------------------------------
+# Test definitions
+# ----------------------------------------------------------
+
 def test_basic_functionality():
     basic_test_pattern(
-                patients_1 = "3_test_patients.csv",
-                expected_linkages = 'zoo,zoo\n'
+            patients_1 = "3_test_patients.csv",
+            expected_linkages = 'zoo,zoo\n'
             )
 
 def test_basic_ordering():
     basic_test_pattern(
-                patients_1 = "3_test_patients.csv",
-                patients_2 = "rev_3_test_patients.csv",
-                expected_linkages = 'zoo,zoo\n0,2\n1,1\n2,0\n'
+            patients_1 = "3_test_patients.csv",
+            patients_2 = "rev_3_test_patients.csv",
+            expected_linkages = 'zoo,zoo\n0,2\n1,1\n2,0\n'
             )
 
 def test_additional_ordering():
     basic_test_pattern(
-                schema = "20_ordering.json",
-                patients_1 = "20_test_matches_a.csv",
-                patients_2 = "20_test_matches_b.csv",
-                expected_linkages = 'simple_synthetic,simple_synthetic\n0,0\n1,2\n2,4\n3,6\n4,8\n5,10\n6,12\n7,14\n8,16\n9,18\n',
+            schema = "20_ordering.json",
+            patients_1 = "20_test_matches_a.csv",
+            patients_2 = "20_test_matches_b.csv",
+            expected_linkages = 'simple_synthetic,simple_synthetic\n0,0\n1,2\n2,4\n3,6\n4,8\n5,10\n6,12\n7,14\n8,16\n9,18\n',
             )
