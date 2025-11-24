a0087ca0cfa46c1268246a29406403d609cdb923
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-11-23 20:34:34 -0500

    Remove print statement

--- src/pprl/tests/utilities.py
+++ src/pprl/tests/utilities.py
@@ -4,5 +4,4 @@ def assert_file_comparison(file_path_1, file_path_2):
 def assert_file_contents(file_path, expected):
     with open(file_path,'r') as file:
         observed = file.read()
-        print(observed)
         assert observed == expected
